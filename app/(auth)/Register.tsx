f0c33684d57c947c37fb88925132d47cbecb928b
tyne <mosqueda.christinereisa04@gmail.com>
2025-07-29 22:16:49 +0800

    reg

--- app/(auth)/Register.tsx
+++ app/(auth)/Register.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native";
+// import { Text, View } from "react-native";
+import { Box } from "@/components/ui/box";
+import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { ScrollView } from "react-native";
+
+const RegisterForm = () => {
+  return <></>;
+};
 
 export default function Register() {
   return (
     <>
-      <View>
-        <Text>This is register page.</Text>
-      </View>
+      <ScrollView>
+        <VStack space="2xl" className="flex space-y-5 items-center m-5">
+          <Box>
+            <Heading></Heading>
+            <Text></Text>
+          </Box>
+        </VStack>
+      </ScrollView>
     </>
   );
 }
